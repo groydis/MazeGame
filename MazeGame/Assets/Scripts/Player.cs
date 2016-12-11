bdc0bdbaea2d2a2e43d05585eaa92746a4d19e00
Greyden Scott <greydenscott@gmail.com>
2016-12-11 21:46:27 +1000

    Add raycast collision dectection and changed movement system to suit

--- MazeGame/Assets/Scripts/Player.cs
+++ MazeGame/Assets/Scripts/Player.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public float battery = 100f;
+	public Light torch; 
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,11 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		battery -= Time.deltaTime;
+		if (battery > 0.0f) {
+			torch.enabled = false;
+		} else {
+			torch.enabled = true;
+		}
 	}
 }
--- MazeGame/Assets/Scripts/PlayerMovement.cs
+++ MazeGame/Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float movementSpeed = 10f;
+	public float movementSpeed = 5f;
+	public float turnCheckDistance = 3f;
+	private Vector3 forward;
+	private Vector3 left = Vector3.left;
+	private Vector3 right = Vector3.right;
+	private Vector3 down = Vector3.back;
+	private Vector3 up = Vector3.forward;
+
+	private void Awake() {
+
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -27,47 +37,89 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		forward = transform.TransformDirection (Vector3.forward) * 10;
+
+		Debug.DrawRay (transform.position, forward, Color.red);
+
+		Debug.DrawRay (transform.position, down, Color.green);
+		Debug.DrawRay (transform.position, up, Color.yellow);
+		Debug.DrawRay (transform.position, left, Color.blue);
+		Debug.DrawRay (transform.position, right, Color.magenta);
+
 		//Player always moves forward
-		transform.Translate (0f, 0f,movementSpeed*Time.deltaTime);
-
-//		#if UNITY_IPHONE
-		if (SwipeManager.IsSwipingLeft()) {
-			transform.rotation = Quaternion.identity;
-			transform.Rotate (0f, -90f, 0f);
-		}
-		if (SwipeManager.IsSwipingRight()) {
-			transform.rotation = Quaternion.identity;
-			transform.Rotate (0f, 90f, 0f);
-		}
-		if (SwipeManager.IsSwipingUp()) {
-			transform.rotation = Quaternion.identity;
-			transform.Rotate (0f, 0f, 0f);
-		}
-		if (SwipeManager.IsSwipingDown()) {
-			transform.rotation = Quaternion.identity;
-			transform.Rotate (0f, 180f, 0f);
+		if (!Physics.Raycast(transform.position, forward, 1)) {
+			transform.Translate (0f, 0f, movementSpeed * Time.deltaTime);
 		}
 
-//		#elif UNITY_EDITOR
-		
-		if (Input.GetKeyDown("left")) {
-			transform.rotation = Quaternion.identity;
-			transform.Rotate (0f, -90f, 0f);
+		//		#if UNITY_IPHONE
+		if (SwipeManager.IsSwipingLeft ()) {
+			TurnLeft ();
 		}
-		if (Input.GetKeyDown ("right")) {
-			transform.rotation = Quaternion.identity;
-			transform.Rotate (0f, 90f, 0f);
+		if (SwipeManager.IsSwipingRight ()) {
+			TurnRight ();
 		}
-		if (Input.GetKeyDown ("up")) {
-			transform.rotation = Quaternion.identity;
-			transform.Rotate (0f, 0f, 0f);
+		if (SwipeManager.IsSwipingUp ()) {
+			TurnUp ();
 		}
-		if (Input.GetKeyDown ("down")) {
-			transform.rotation = Quaternion.identity;
-			transform.Rotate (0f, 180f, 0f);
+		if (SwipeManager.IsSwipingDown ()) {
+			TurnDown ();
 		}
 
-//		#endif
-
+		//		#elif UNITY_EDITOR
+		if (!Physics.Raycast (transform.position, left, turnCheckDistance)) {
+			if (Input.GetKeyDown ("left")) {
+				TurnLeft ();
+			}
+		}
+		if (!Physics.Raycast (transform.position, right, turnCheckDistance)) {
+			if (Input.GetKeyDown ("right")) {
+				TurnRight ();
+			}
+		}
+		if (!Physics.Raycast (transform.position, up, turnCheckDistance)) {
+			
+			if (Input.GetKeyDown ("up")) {
+				TurnUp ();
+			}
+		}
+		if (!Physics.Raycast (transform.position, down, turnCheckDistance)) {
+			if (Input.GetKeyDown ("down")) {
+				TurnDown ();
+			}
+		}
+		//		#endif
+		if (Physics.Raycast (transform.position, left, turnCheckDistance)) {
+			print ("Can't turn left");
+		}
+		if (Physics.Raycast (transform.position, right, turnCheckDistance)) {
+			print ("Can't turn right");
+		}
+		if (Physics.Raycast (transform.position, up, turnCheckDistance)) {
+			print ("Can't turn up");
+		}
+		if (Physics.Raycast (transform.position, down, turnCheckDistance)) {
+			print ("Can't turn down");
+		}
 	}
+		
+	void TurnLeft() {
+		transform.rotation = Quaternion.identity;
+		transform.Rotate (0f, -90f, 0f);
+	}
+
+	void TurnRight() {
+		transform.rotation = Quaternion.identity;
+		transform.Rotate (0f, 90f, 0f);
+	}
+
+	void TurnUp() {
+		transform.rotation = Quaternion.identity;
+		transform.Rotate (0f, 0f, 0f);
+	}
+
+	void TurnDown() {
+		transform.rotation = Quaternion.identity;
+		transform.Rotate (0f, 180f, 0f);
+	}
+		
 }
