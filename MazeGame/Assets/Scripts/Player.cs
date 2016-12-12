a4edeeeb253a0feb92557d21d14c5da9d67dfae3
Greyden Scott <greydenscott@gmail.com>
2016-12-12 12:33:39 +1000

    Added TorchControl script to drain torch battery and intitiate a flicker when battery is low

--- MazeGame/Assets/Scripts/Player.cs
+++ MazeGame/Assets/Scripts/Player.cs
@@ -3,21 +3,18 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	public float battery = 100f;
-	public Light torch; 
+	public static float batteryCharge;
+	public static float batteryDrainRate;
 
 	// Use this for initialization
 	void Start () {
+		batteryCharge = 10f;
+		batteryDrainRate = 1f;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		battery -= Time.deltaTime;
-		if (battery > 0.0f) {
-			torch.enabled = false;
-		} else {
-			torch.enabled = true;
-		}
+
 	}
 }
---
+++ MazeGame/Assets/Scripts/TorchControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorchControl : MonoBehaviour {
+
+	public float waitTime = 10.0f;
+	public float minFlickerSpeed = 0.1f;
+	public float maxFlickerSpeed = 1.0f;
+	public float lightIntensityReduction = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (BatteryDrain ());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Player.batteryCharge > 8f) {
+			GetComponent<Light> ().intensity = 5f;
+		}
+	
+	}
+
+	IEnumerator BatteryDrain() {
+		while (Player.batteryCharge !=0) {
+			Player.batteryCharge -= Player.batteryDrainRate;
+			print ("Battery Charge : " + Player.batteryCharge);
+			if (Player.batteryCharge <= 8f && Player.batteryCharge > 4f) {
+				GetComponent<Light> ().intensity -= 1.0f;
+				print ("Reducing Intensity by : " + lightIntensityReduction);
+			}
+			while (Player.batteryCharge <= 2.0f) {
+				GetComponent<Light> ().enabled = true;
+				print ("Torch On");
+				yield return new WaitForSeconds (Random.Range (minFlickerSpeed, maxFlickerSpeed));
+				GetComponent<Light> ().enabled = false;
+				print ("Torch Off");
+				yield return new WaitForSeconds (Random.Range (minFlickerSpeed, maxFlickerSpeed));
+			}
+			yield return new WaitForSeconds(waitTime);
+				
+		}
+	
+	}
+}
